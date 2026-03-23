c7234581944ec797e2fadb76de0fea8a994a7af0
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-23 20:07:05 +0530

    feat: Page loader completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,9 @@ import PageLoader from './components/PageLoader'
 import ScrollTop from './components/ScrollTop'
 import NotFound from './components/NotFound'
 
-const Home = lazy(() => import('./pages/Home'))
+const Home = lazy(() =>
+  new Promise(resolve => setTimeout(resolve, 100000)) 
+    .then(() => import('./pages/Home')))
 const About = lazy(() => import('./pages/About'))
 const Activities = lazy(() => import('./pages/Activities'))
 const Events = lazy(() => import('./pages/Events'))
@@ -20,35 +22,36 @@ const Faculties = lazy(() => import('./pages/Faculties'))
 const Management = lazy(() => import('./pages/Management'))
 
 function App() {
-  <AppProvider>
-    <BrowserRouter>
+  return(
+    <AppProvider>
+      <BrowserRouter>
 
-      <ScrollTop>
-        
-        <Navbar />
-        <Drawer />
+          <Navbar />
+        <ScrollTop />
+          
+          <Drawer />
 
-        <Suspense fallback={<PageLoader />}>
-          <Routes>
-            <Route path={ROUTES.HOME} element={<Home />} />
-            <Route path={ROUTES.ABOUT} element={<About />} />
-            <Route path={ROUTES.ACTIVITIES} element={<Activities />} />
-            <Route path={ROUTES.EVENTS} element={<Events />} />
-            <Route path={ROUTES.FACULTIES} element={<Faculties />} />
-            <Route path={ROUTES.MANAGEMENT} element={<Management />} />
+          <Suspense fallback={<PageLoader />}>
+            <Routes>
+              <Route path={ROUTES.HOME} element={<Home />} />
+              <Route path={ROUTES.ABOUT} element={<About />} />
+              <Route path={ROUTES.ACTIVITIES} element={<Activities />} />
+              <Route path={ROUTES.EVENTS} element={<Events />} />
+              <Route path={ROUTES.FACULTIES} element={<Faculties />} />
+              <Route path={ROUTES.MANAGEMENT} element={<Management />} />
 
-            <Route path='/home' element={<Navigate to="/" replace />} />
+              <Route path='/home' element={<Navigate to="/" replace />} />
 
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </Suspense>
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </Suspense>
 
-        <Footer />
+          <Footer />
 
-      </ScrollTop>
 
-    </BrowserRouter>
-  </AppProvider>
+      </BrowserRouter>
+    </AppProvider>
+  )
 }
 
 export default App
--- src/components/PageLoader.jsx
+++ src/components/PageLoader.jsx
@@ -1,1 +1,9 @@
-export default function PageLoader() {}
+export default function PageLoader() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-40 h-40 animate-pulse">
+                <img src="/src/assets/images/shia-logo-p.png" alt=""  />
+            </div>
+        </div>
+    )
+}
