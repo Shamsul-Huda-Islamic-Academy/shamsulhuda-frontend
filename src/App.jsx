85ac0e5cfb663bab9be78e18becbcd9e7dc3ddc9
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-23 12:45:00 +0530

    feat: App.jsx page completed with routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,54 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import { AppProvider } from './context/AppContext'
 import { ROUTES } from './constants/routes'
 
+import Navbar from './components/Navbar'
+import Drawer from './components/Drawer'
+import Footer from './components/Footer'
+import PageLoader from './components/PageLoader'
+import ScrollTop from './components/ScrollTop'
+import NotFound from './components/NotFound'
 
-
-// import Home from './pages/Home'
-// import About from './pages/About'
-// import Activities from './pages/Activities'
-// import Events from './pages/Events'
-// import Faculties from './pages/Faculties'
-// import Management from './pages/Management'
-
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+const Activities = lazy(() => import('./pages/Activities'))
+const Events = lazy(() => import('./pages/Events'))
+const Faculties = lazy(() => import('./pages/Faculties'))
+const Management = lazy(() => import('./pages/Management'))
 
 function App() {
+  <AppProvider>
+    <BrowserRouter>
 
+      <ScrollTop>
+        
+        <Navbar />
+        <Drawer />
+
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.ABOUT} element={<About />} />
+            <Route path={ROUTES.ACTIVITIES} element={<Activities />} />
+            <Route path={ROUTES.EVENTS} element={<Events />} />
+            <Route path={ROUTES.FACULTIES} element={<Faculties />} />
+            <Route path={ROUTES.MANAGEMENT} element={<Management />} />
+
+            <Route path='/home' element={<Navigate to="/" replace />} />
+
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
+
+        <Footer />
+
+      </ScrollTop>
+
+    </BrowserRouter>
+  </AppProvider>
 }
 
 export default App
