23c780d6290f238c508a6987c18fcfc1476b1e89
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-29 11:59:32 +0530

    feat: Event page designed

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -3,6 +3,21 @@ import { eventsData } from "../constants/eventData";
 
 export default function Activities() {
     return (
-        <section className="px-4 mx-12 mx-auto md:px-10"></section>
+        <section className="px-4 py-12 mx-auto -12 md:px-10">
+            <div className="mb-10">
+                <h1 className="text-3xl font-bold text-gray-900 md:text-4xl">
+                    Activities
+                </h1>
+            </div>
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+                {eventsData.map((event) => (
+                    <Card
+                        key={event.id}
+                        title={event.name}
+                        {...event}  
+                    />
+                ))} 
+            </div>
+        </section>
     )
 }
