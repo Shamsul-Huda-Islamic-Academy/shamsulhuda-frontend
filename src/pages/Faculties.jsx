f48e994dd6cc76555843b3e491c141a367bd1db6
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-03-30 10:53:12 +0530

    feat: Faculty not found info uploaded

--- src/pages/Faculties.jsx
+++ src/pages/Faculties.jsx
@@ -1,5 +1,6 @@
 import { ProfileCard } from '../components/ProfileCard'
 import { facultiesData } from '../constants/faculties'
+import { CalendarX } from "lucide-react";
 
 export default function Faculties() {
     return (
@@ -8,14 +9,28 @@ export default function Faculties() {
             <div className="mb-10">
                 
                 <h1 className="text-3xl font-bold text-gray-900 md:text-4xl">
-                    Faculty Members
+                    Faculties
                 </h1>
             </div>
 
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3">
-                {facultiesData.map((faculty) => (
+                {facultiesData.length === 0 ? (
+                    <div className="flex flex-col items-center justify-center col-span-3 py-24 text-center">
+                    <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-green-50">
+                        <CalendarX size={28} className="text-[#4A9C59]" />
+                    </div>
+                    <h3 className="mb-1 text-lg font-semibold text-gray-800">
+                        No Faculty Uploaded
+                    </h3>
+                    <p className="text-sm text-gray-400">
+                        Check back soon — new faculties will appear here.
+                    </p>
+                </div>
+                ) : (
+                facultiesData.map((faculty) => (
                     <ProfileCard key={faculty.id} {...faculty} />
-                ))}
+                ))
+            )}
             </div>
 
         </section>
